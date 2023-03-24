6919eb5b3547c6b0899a00e1fd53bb467095af44
solovieva-v <solovieva.veronika00@mail.ru>
2023-03-25 00:27:47 +0300

    end IntentFilter

---
+++ Lesson2/intentfilter/src/main/java/ru/mirea/solovievava/intentfilter/IntentActivity.java
@@ -0,0 +1,31 @@
+package ru.mirea.solovievava.intentfilter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class IntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent);
+    }
+    public void onButtonClickBTN(View view)
+    {
+        Uri address = Uri.parse("https://www.mirea.ru/");
+        Intent openLinkIntent = new Intent(Intent.ACTION_VIEW, address);
+        startActivity(openLinkIntent);
+    }
+
+    public void onAnotherBTN(android.view.View view){
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "MIREA");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "Соловьева Вероника Антоновна");
+        startActivity(Intent.createChooser(shareIntent, "МОИ ФИО"));
+    }
+}
